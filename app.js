c465da38db8a2738aa2577e29cb15dc0eb9aabcb
Jalp Patel <jalpvpatel12042007@gmail.com>
2026-07-14 13:17:47 +0530

    Add current weather search, geolocation and code refactoring

--- app.js
+++ app.js
@@ -20,6 +20,36 @@ const forecastContainer=document.getElementById("forecast-container");
 
 const BASE_URL="https://api.openweathermap.org/data/2.5/weather";
 
+function updateWeatherUI(data) {
+    cityName.textContent=data.name;
+    temperature.textContent=`${Math.round(data.main.temp)}°C`;
+        
+    const weatherDescription = data.weather[0].description;
+    description.textContent =
+    weatherDescription.charAt(0).toUpperCase() +
+    weatherDescription.slice(1);
+
+    const iconCode=data.weather[0].icon;
+    weatherIcon.src=`https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    weatherIcon.alt = data.weather[0].description;
+
+    feelsLike.textContent=`${Math.round(data.main.feels_like)}°C`;
+    humidity.textContent=`${data.main.humidity}%`;
+    windSpeed.textContent = `${Math.round(data.wind.speed)} m/s`;
+    visibility.textContent = `${Math.round(data.visibility / 1000)} km`;
+
+    const today=new Date();
+    const formattedDate = today.toLocaleDateString("en-IN", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+        });
+    date.textContent = formattedDate;
+
+    error.classList.add("hidden");
+}
+
 async function getWeather(city) {
     searchBtn.disabled = true;
     loading.classList.remove("hidden");
@@ -40,34 +70,8 @@ async function getWeather(city) {
             return;
         }
 
-        cityName.textContent=data.name;
-        temperature.textContent=`${Math.round(data.main.temp)}°C`;
-        
-        const weatherDescription = data.weather[0].description;
-        description.textContent =
-        weatherDescription.charAt(0).toUpperCase() +
-        weatherDescription.slice(1);
+        updateWeatherUI(data);
 
-        const iconCode=data.weather[0].icon;
-        weatherIcon.src=`https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-        weatherIcon.alt = data.weather[0].description;
-
-        feelsLike.textContent=`${Math.round(data.main.feels_like)}°C`;
-        humidity.textContent=`${data.main.humidity}%`;
-        windSpeed.textContent = `${Math.round(data.wind.speed)} m/s`;
-        visibility.textContent = `${Math.round(data.visibility / 1000)} km`;
-
-
-        const today=new Date();
-        const formattedDate = today.toLocaleDateString("en-IN", {
-            weekday: "long",
-            day: "numeric",
-            month: "long",
-            year: "numeric"
-        });
-        date.textContent = formattedDate;
-
-        error.classList.add("hidden");
 
     } catch (err) {
     console.error(err);
@@ -103,3 +107,50 @@ cityInput.addEventListener("keydown", (event) => {
         searchWeather();
     }
 });
+
+locationBtn.addEventListener("click", () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        
+        getWeatherByCoords(latitude, longitude);
+
+    },(err) => {
+        console.log(err);
+    });
+});
+
+async function getWeatherByCoords(latitude, longitude) {
+    searchBtn.disabled = true;
+    loading.classList.remove("hidden");
+
+    const url =
+        `${BASE_URL}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Something went wrong");
+        }
+
+        const data = await response.json();
+
+        if (Number(data.cod) !== 200) {
+            error.textContent = data.message;
+            error.classList.remove("hidden");
+            return;
+        }
+
+        updateWeatherUI(data);
+
+    } catch (err) {
+        console.error(err);
+        error.textContent = "Failed to fetch weather data. Please try again.";
+        error.classList.remove("hidden");
+    } finally {
+        searchBtn.disabled = false;
+        loading.classList.add("hidden");
+    }
+}
